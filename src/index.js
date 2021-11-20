847eafd7cdb2cf4ef8c856b451ea6517acd8dab2
Dakota Leonard <dakotaleonard.dev@gmail.com>
2021-11-20 16:56:43 -0500

    Add preload and create function

--- src/index.js
+++ src/index.js
@@ -10,8 +10,34 @@ const config = {
 
   //Physics to use for gravity. Arcade is a good default.
   physics: {
+    //Arcade physicis plugin, manages physicis simulations (gravity/velocity/etc)
     default: 'arcade',
   },
+
+  //What is seen on the screen.
+  scene: {
+    //Functions called in this order: preload, create, update
+
+    //In preload we load assets such as images, music, animations, etc
+    preload,
+
+    //Initializes application
+    create,
+  },
 };
+
+//This context during preload is Scene. Contains functions and properties we can use.
+function preload() {
+  //this.load.image(What key we want to refer to the file as, path to file)
+  this.load.image('sky', 'assets/sky.png');
+}
+
+//Now we add the image to render
+function create() {
+  //this.add.image(x coordinate, y coordinate, what key we are referring to the file as)
+
+  //KEEP IN MIND THE CENTER OF THE IMAGE IS INSERTED WHERE THE COORDINATES ARE SPECIFIED
+  this.add.image(config.width / 2, config.height / 2, 'sky');
+}
 
 new Phaser.Game(config);
